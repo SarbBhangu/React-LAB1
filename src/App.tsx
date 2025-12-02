813775967905e49f9ee66531c77e90b9eeadf9c4
SarbBhangu <Bhangu513@gmail.com>
2025-12-02 15:16:02 -0500

    added AlertBox example usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { AlertBox } from "./components/AlertBox/AlertBox";
+
+<AlertBox
+  type="success"
+  message="Your profile has been updated successfully!"
+  onClose={() => alert('Alert closed')}
+>
+  <p className="text-sm">You can now continue using the application.</p>
+</AlertBox>
 
 
 
